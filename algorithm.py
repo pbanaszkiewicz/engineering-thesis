5644517138509176d93ecb0632658693bd42d65b
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-08-09 17:10:52 +0200

    Basic algorithm layout using finite state machine
    There's a number of states (none implemented); basically a Python
    version of provided state-chart.  It's still a subject to change.

---
+++ algorithm.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Algorithm scaffolding (based on a state machine).
+"""
+
+
+def turn_off():
+    """Cut off the power from the batteries to the engine."""
+    pass
+
+
+def turn_on():
+    """Bring back the power to the engine."""
+    pass
+
+
+if __name__ == '__main__':
+    state = START
+
+    try:
+        while True:
+            if state == START:
+                pass
+            elif state == CONNECTION_WAIT:
+                pass
+            elif state == CONNECTED:
+                pass
+            elif state == DIAGNOSTIC_DATA:
+                pass
+            elif state == PING:
+                pass
+            elif state == PONG_WAIT:
+                pass
+            elif state == STOP:
+                pass
+            elif state == RESET:
+                pass
+            else:
+                # error?
+                pass
+    except KeyboardInterrupt:
+        print('Transmisja przerwana!')
+    finally:
+        pass
