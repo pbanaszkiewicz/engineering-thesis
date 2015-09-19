cf05d719ef980e7288876d04db842eb22447fcc4
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2015-09-19 22:45:07 +0200

    Working stop-reset && ping-pong

--- moduleA.py
+++ moduleA.py
@@ -1,21 +1,52 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import socket
 import time
 
 import RPi.GPIO as GPIO
 
-# globals, we love them, don't we
+# Globals. We love them, don't we?
 STOP_SEND = False
 
+# pin setup
 STOP_PIN = 17
 RXTX_PIN = 27  # 21 in rev1
+
+# remote setup
+HOST = "moduleB"
+PORT = 8888
 
 
 def stop_btn(channel):
     global STOP_SEND
-    print "[PRESSED] stop"
     STOP_SEND = True
+    print("[PRESSED] stop")
 
 
-if __name__ == '__main__':
+def ping(stop=False):
+    data = ""
+    if stop:
+        print("[SENDING] stop & ping")
+        data = "stop,ping"
+    else:
+        print("[SENDING] ping")
+        data = "ping"
+
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((HOST, PORT))
+        sock.sendall(bytes(data + "\n", "utf-8"))
+
+        rv = str(sock.recv(1024), "utf-8")
+
+    finally:
+        sock.close()
+
+    return rv
+
+
+def setup():
     GPIO.setmode(GPIO.BCM)  # SoC numbering of pins
 
     GPIO.setup(STOP_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -24,20 +55,18 @@ if __name__ == '__main__':
     GPIO.add_event_detect(STOP_PIN, GPIO.RISING, callback=stop_btn,
                           bouncetime=300)
 
+
+if __name__ == '__main__':
+    setup()
+
     try:
         while True:
             time.sleep(1)
 
             GPIO.output(RXTX_PIN, True)
-            if STOP_SEND:
-                # send PING and STOP
-                STOP_SEND = False
-                print "[SENDING] stop & ping"
-
-            else:
-                # send only PING
-                print "[SENDING] ping"
-
+            result = ping(stop=STOP_SEND)
+            STOP_SEND = False  # reset anyway
+            print("[RESPONSE] {result}".format(result=result))
             GPIO.output(RXTX_PIN, False)
 
     except KeyboardInterrupt:
--- moduleB.py
+++ moduleB.py
@@ -1,38 +1,71 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import socketserver
 import time
 
 import RPi.GPIO as GPIO
 
+STATUS = True
+
+# pin setup
 RESET_PIN = 22
 RXTX_PIN = 21  # 27 in rev2
 STATUS_PIN = 17
 
+# local setup
+HOST = "0.0.0.0"
+PORT = 8888
+
 
 def reset_btn(channel):
-    print "[PRESSED] reset"
+    global STATUS
+    print("[PRESSED] reset")
+    STATUS = True
+    GPIO.output(STATUS_PIN, STATUS)
 
 
-if __name__ == '__main__':
+class Server(socketserver.StreamRequestHandler):
+    def handle(self):
+        global STATUS
+
+        GPIO.output(RXTX_PIN, True)
+
+        self.data = self.rfile.readline().strip()
+        if b"stop" in self.data:
+            STATUS = False
+            GPIO.output(STATUS_PIN, STATUS)
+
+        print("[RECEIVED] \"{}\" from {}".format(self.data,
+                                                 self.client_address[0]))
+        self.wfile.write(self.data)
+
+        GPIO.output(RXTX_PIN, False)
+
+
+def setup():
     GPIO.setmode(GPIO.BCM)  # SoC numbering of pins
 
     GPIO.setup(RESET_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-    GPIO.setup(STATUS_PIN, GPIO.OUT)
+    GPIO.setup(STATUS_PIN, GPIO.OUT, initial=STATUS)
     GPIO.setup(RXTX_PIN, GPIO.OUT)
 
     GPIO.add_event_detect(RESET_PIN, GPIO.RISING, callback=reset_btn,
                           bouncetime=300)
 
-    try:
-        while True:
-            time.sleep(1)
+    server = socketserver.TCPServer((HOST, PORT), Server)
+    return server
 
-            GPIO.output(RXTX_PIN, True)
-            GPIO.output(STATUS_PIN, True)
-            time.sleep(0.001)
-            GPIO.output(RXTX_PIN, False)
-            GPIO.output(STATUS_PIN, False)
+
+if __name__ == '__main__':
+    server = setup()
+
+    try:
+        server.serve_forever()
 
     except KeyboardInterrupt:
         pass
+
     finally:
         GPIO.cleanup()
